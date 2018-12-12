4c87d82c8fff9d986c593f60803acb947162a768
dreamcxy <dreamcxy@gmail.com>
2018-12-12 11:46:04 +0800

    from mac 1212

--- scripts/GroupAllCameraStrings.py
+++ scripts/GroupAllCameraStrings.py
@@ -1,6 +1,8 @@
+# encoding=utf-8
 import os
 import re
 import platform
+import xml.etree.ElementTree as et
 
 STRINGSDIR = "C:/Users/dreamcxy/Desktop/Security/SilentCamera/codes/Jeb/strings/" if platform.system(
 ) == 'Windows' else "/Users/chenxiaoyu/Desktop/Project/Jeb/strings/"
@@ -10,23 +12,34 @@ def loadStringsSuitable():
     stringsList = []
     stringsFiles = os.listdir(STRINGSDIR)
     while stringsFiles:
-        stringFile = stringsFiles.pop()
-
+        stringFile = STRINGSDIR + stringsFiles.pop()
+        print stringFile
+        stringsList += extractSuitableStringFromFile(stringFile)
     return stringsList
 
 
 def extractSuitableStringFromFile(stringFile):
     stringsList = []
     with open(stringFile) as strings:
-        for line in strings.readlines():
-            stringsList.append(re.match("<string[^>]*>([^<]*)</string>", line.strip()))
-    return stringsList
+        content = strings.read()
+    content = re.sub(u'[\x00-\x08\x0b-\x0c\x0e-\x1f]+', u"", content)
+    try:
+        root = et.fromstring(content)
+        print len(root)
+        for i in range(0, len(root)):
+            if root[i].text:
+                stringsList.append(root[i].text.encode("utf-8"))
+        return stringsList    
+    except et.ParseError:
+        return []
 
 
 def main():
-    stringsList = extractSuitableStringFromFile(
-        "/Users/chenxiaoyu/Desktop/Project/Jeb/strings/a201706011153.languang.vpn 1.0.5.1 11 .apk.txt")
-    for string in stringsList:
-        print string
+    stringList = loadStringsSuitable()
+    print len(stringList)
+    while stringList:
+        print stringList.pop()
+
+
 if __name__ == '__main__':
     main()
