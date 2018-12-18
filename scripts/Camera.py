9bddfcea472cfbc77a8e365b8e0ce0bbb5d662c2
dreamcxy <dreamcxy@gmail.com>
2018-12-18 21:09:30 +0800

    from mac

--- scripts/Camera.py
+++ scripts/Camera.py
@@ -1,7 +1,7 @@
-#encoding=utf-8
+# coding=utf-8
 from enum import Enum
 
-class Camera(Enum):
+class Camera_EN(Enum):
     camera = "camera"
     photo = "photo"
     photo_alias_1 = "picture"
@@ -10,10 +10,22 @@ class Camera(Enum):
     scan = "scan"
     scanner = "scanner"
     qrcode = "qrcode"
-    
+
+
+class Camera_CN(Enum):
     camera_cn = "照相机"
     camera_cn_alias_1 = "摄像头"
+    camera_cn_alias_2 = "相机"
     photo_cn_alias = "照片"
     video_cn = "视频"
+    qrcode_cn = "二维码"
+    scan_cn = "扫描"
 
-    
+
+
+def main():
+    for cn in Camera_CN:
+        print cn.value
+
+if __name__ == '__main__':
+    main()
--- scripts/GroupAllCameraStrings.py
+++ scripts/GroupAllCameraStrings.py
@@ -3,12 +3,11 @@ import os
 import re
 import platform
 import xml.etree.ElementTree as et
-import Camera
+from Camera import Camera_CN, Camera_EN
 
 
 STRINGSDIR = "C:/Users/dreamcxy/Desktop/Security/SilentCamera/codes/Jeb/strings/" if platform.system(
 ) == 'Windows' else "/Users/chenxiaoyu/Desktop/Project/Jeb/strings/"
-
 
 def load_strings_suitable():
     strings_list = []
@@ -30,21 +29,34 @@ def extract_suitable_string_from_file(string_file):
         print len(root)
         for i in range(0, len(root)):
             if root[i].text:
-                strings_list.append(root[i].text)
-                if distinguish_cn(root[i].text):
-                    extract_to_file(root[i].text, "")
-                else:
-                    extract_to_file(root[i].text, "")
-                    pass
-                # stringList.append(root[i].text.encoding("utf-8"))
+                # strings_list.append(root[i].text)
+                root_text = root[i].text
+                if distinguish_cn(root_text) and if_related_camera_cn(root_text.encode("utf-8")):
+                    extract_to_file(root_text, "strings_cn.txt")
+                elif if_related_camera_en(root_text):
+                    extract_to_file(root_text, "strings_en.txt")
+                strings_list.append(root_text)
         return strings_list
     except et.ParseError:
         return []
 
+def if_related_camera_cn(text):
+    for cn in Camera_CN:
+        if cn.value in text:
+            return True
+    return False
 
-def extract_to_file(strings, output_filename):
-    with open(output_filename, "w") as output_file:
-        output_file.write(strings)
+def if_related_camera_en(text):
+    for en in Camera_EN:
+        if en.value in text:
+            return True
+    return False
+
+
+def extract_to_file(text, output_filename):
+    with open(output_filename, "a") as output_file:
+        # output_file.write(strings)
+        output_file.write(text.encode("utf-8"))
 
 
 def distinguish_cn(string_text):
@@ -56,11 +68,11 @@ def distinguish_cn(string_text):
 def main():
     stringList = load_strings_suitable()
     print "length: " + str(len(stringList))
-    while stringList:
-        try:
-            print stringList.pop()
-        except IOError:
-            continue
+    # while stringList:
+    #     try:
+    #         print stringList.pop()
+    #     except IOError:
+    #         continue
 
 
 if __name__ == '__main__':
